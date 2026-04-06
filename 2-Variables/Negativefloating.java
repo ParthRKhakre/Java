144eed5ebe3e9a914a9b50f1dbade30e6379c5f6
ParthRKhakre <parthrkhakre@gmail.com>
2026-04-07 01:44:50 +0530

    4-5

--- 2-Variables/Negativefloating.java
+++ 2-Variables/Negativefloating.java
@@ -1,12 +1,29 @@
 public class Negativefloating {
-    public static void main(String arg[]){
+    public static void main(String arg[]) {
+        // Due to this loss in data Java Introduced BIG_DECIMAL
+
         float c = 9.25f;
         float f = 0.9f;
 
-        System.out.printf("%.20f %n",c);
-        System.out.printf("%.20f %n",f);
+        System.out.printf("%.20f %n", c);
+        System.out.printf("%.20f %n", f);
+
+        // Implicit Conversion (byte to int)
+        byte x = 10;
+        int q;
+        q = x;
+        System.out.println("byte to int " + q);
+
+        // Character to int
+        char v = 'a';
+        int m = v;
+        System.out.println("Character to Integer " + m);
+
+        // Explicit Conversion
+        int a = 300;
+        byte i;
+        i = (byte) a; // Casting is required
+        System.out.println("int to byte " + i);
+
     }
 }
-
-// How Negative Values are stored in Memory
-
