b5d8de55aec4e2f7beea6bc4f0deaa9c107ee9eb
ParthRKhakre <parthrkhakre@gmail.com>
2026-04-06 23:55:52 +0530

    ok

---
+++ 2-Variables/Negativefloating.java
@@ -0,0 +1,12 @@
+public class Negativefloating {
+    public static void main(String arg[]){
+        float c = 9.25f;
+        float f = 0.9f;
+
+        System.out.printf("%.20f %n",c);
+        System.out.printf("%.20f %n",f);
+    }
+}
+
+// How Negative Values are stored in Memory
+
