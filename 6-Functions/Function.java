c24b24aa56ad949448a0fc3120d5640f60a44703
ParthRKhakre <parthrkhakre@gmail.com>
2026-05-24 23:34:34 +0530

    functions

--- 6-Functions/Function.java
+++ 6-Functions/Function.java
@@ -1,5 +1,10 @@
 public class Function {
+    public static double add(int a,double b){
+        return (float) a + b;
+    }
+
     public static void main(String[] args) {
-        
+        double sum = add(10,9.5);
+        System.out.println(sum);
     }
 }
